bc7983df55b83e794204d91516aba239cf0f5f61
Arsbars24 <arsik.1104@mail.ru>
2024-09-22 12:17:10 +0500

    initial commit

---
+++ src/main/java/com/coursework/courseworkapplication/CourseworkapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.courseworkapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursework/courseworkapplication/CourseworkapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.courseworkapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
